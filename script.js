6834b4b1ff3857ce55a80f47769d67bb59f5a509
xeus6969 <121609783+xeus6969@users.noreply.github.com>
2023-09-01 14:54:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const result = document.getElementById("point");
 
 calculateButton.addEventListener("click", function() {
   result.innerHTML = Math.floor(Math.random() * 100 + 1);
+  const darkModeButton = document.getElementById("dark-mode-button");
+const body = document.body;
+
+darkModeButton.addEventListener("click", function() {
+  body.classList.toggle("dark-mode");
+});
 });
